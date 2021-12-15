f93bfdb33448a225c0773c0491cdf0c94cde5301
Laavart <laavart11@gmail.com>
2021-12-15 06:50:27 +0530

    commit

---
+++ src/d1215.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class d1215 {
+
+    static void maneuver(int m, int n){
+        //Code
+    }
+
+    public static void main(String[] Args) throws Exception{
+        BufferedReader bRead = new BufferedReader(new InputStreamReader(System.in));
+
+        maneuver(Integer.parseInt(bRead.readLine()), Integer.parseInt(bRead.readLine()));
+    }
+}
