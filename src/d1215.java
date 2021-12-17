8b3fcda496a23d4a31e6083c88d76da1fd1f94bb
Laavart <laavart11@gmail.com>
2021-12-17 14:18:54 +0530

    commit

--- src/d1215.java
+++ src/d1215.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class d1215 {
 
+    Stack<Integer> x = new Stack<>();
+    Stack<Integer> y = new Stack<>();
     static void maneuver(int m, int n){
-        //Code
+        if(m>0 && n>0){
+            //Code
+        }
     }
 
     public static void main(String[] Args) throws Exception{
