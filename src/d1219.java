941a87661bac824ec9ab415a5d4fddfa6f681ad1
Laavart <laavart11@gmail.com>
2021-12-19 08:38:01 +0530

    commit

---
+++ src/d1219.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+public class d1219 {
+
+    static void Combinations(Vector<int[]> v, int a[], int n){
+        //Code
+    }
+
+    public static void main(String[] Args) throws Exception{
+        BufferedReader bRead = new BufferedReader(new InputStreamReader(System.in));
+        Vector<int[]> v = new Vector<>();
+        int[] a = {1,2,3,4,5};
+        Combinations(v, a, Integer.parseInt(bRead.readLine()));
+    }
+}
