797e5e6f95b3ded3291d6fc4af940ee92a8d571c
Laavart <laavart11@gmail.com>
2021-12-20 14:00:11 +0530

    commit

--- src/d1220.java
+++ src/d1220.java
@@ -2,11 +2,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class d1220 {
+
+    // This function removes vowels from the sentence using recursions.
+    // Does not use HashSet because it calls a library to the build and uses more memory.
     static void removeVowels(String s, StringBuilder r, int n){
+        // Checking that index value does not exceed
         if(n < s.length()){
+            // Storing character at index, so we don't have to use function everytime
             char c = s.charAt(n);
+            // Checking for vowels
             if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U')
+                // If not a vowel just append it to result
                 r.append(c);
+            // Repeat for next index
             removeVowels(s, r, ++n);
         }
     }
