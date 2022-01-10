9cc4ab7c0a8fc740594ea34fdc2d044f8949dad7
laava <laavart11@gmail.com>
2022-01-10 16:05:10 +0530

    Commit

--- src/pb20220109.java
+++ src/pb20220109.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.Vector;
 
 public class pb20220109 {
 
@@ -15,22 +17,49 @@ public class pb20220109 {
         }
     }
 
-    static void expVal(String s){
-        int l = s.length(), h=0;
+    static Vector<String> v = new Vector<>();
+
+    static Stack<Character> validExp(String e){
+        Stack<Character> s = new Stack<>();
+        int l = e.length();
         for(int i=0; i<l; i++){
-            char c = s.charAt(i);
-            if(c == '(') h++;
-            else if(c ==')') h--;
-            if(h<0) cPar(s.substring(i)+s.substring(i+1,l));
+            char c = e.charAt(i);
+            if(c=='(') s.push(c);
+            else if(c==')')
+                if(s.isEmpty() || c == s.peek()) s.push(c);
+                else s.pop();
         }
-        if(h>0) oPar(s);
+        return s;
     }
 
-    static void cPar(String s){
-
+    static String removeParan(String e, Stack<Character> o, int n){
+        StringBuilder sb = new StringBuilder(e);
+        Stack<Character> s = (Stack<Character>) o.clone();
+        while(n>=0 && !s.isEmpty()){
+            char c = sb.charAt(n);
+            if(c == s.peek()){
+                sb.deleteCharAt(n);
+                s.pop();
+                n--;
+            }
+            n--;
+        }
+        e = sb.toString();
+        if(validExp(e).isEmpty() && (v.isEmpty() || !v.contains(e)) ){
+            v.add(e);
+            return e;
+        }
+        else return "";
     }
 
-    static void oPar(String s){
+    static void expVal(String e1){
+        Stack<Character> s = validExp(e1);
+        int l = e1.length();
+        if(!s.isEmpty())
+            for(int i=l-1; i>=0; i--){
+                String e2 = removeParan(e1, s, i);
+                if(!e2.equals("")) System.out.println(e2);
+            }
 
     }
 
@@ -39,6 +68,7 @@ public class pb20220109 {
 
         //perfectNumber(Integer.parseInt(bRead.readLine().trim()), Integer.parseInt(bRead.readLine().trim()));
 
-        expVal(bRead.readLine());
+        expVal("(A)+(B))*(C)");
+        expVal(")X)Y(Z(");
     }
 }
