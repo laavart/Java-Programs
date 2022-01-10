1aac483d1f95cb95180c960cae6c17d64f259277
laava <laavart11@gmail.com>
2022-01-10 10:46:05 +0530

    Commit

--- src/pb20220109.java
+++ src/pb20220109.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class pb20220109 {
-    static BufferedReader bRead = new BufferedReader(new InputStreamReader(System.in));
 
     static void perfectNumber(int l, int u){
         while(l<=u){
@@ -15,7 +14,23 @@ public class pb20220109 {
         }
     }
 
+    static void expVal(String s){
+        int l = s.length(), h=0, e=0;
+        for(int i=0; i<l; i++){
+            char b=0, c=0, d=0;
+            if(i!=0) b = s.charAt(i-1);
+            c = s.charAt(i);
+            if(i!=l-1) d = s.charAt(i+1);
+            if(c == '(') h++;
+            else if(c ==')') h--;
+        }
+    }
+
     public static void main(String[] Args) throws Exception{
+        BufferedReader bRead = new BufferedReader(new InputStreamReader(System.in));
+
         perfectNumber(Integer.parseInt(bRead.readLine()), Integer.parseInt(bRead.readLine()));
+
+        expVal(bRead.readLine());
     }
 }
