156281590a7adeb03b0648d4712d42be7059240d
laava <laavart11@gmail.com>
2022-01-20 19:56:01 +0530

    Commit

---
+++ src/s20220120.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class s20220120 {
+    public static void main(String[] Args) throws IOException{
+        BufferedReader bRead = new BufferedReader(new InputStreamReader(System.in));
+        try(FileOutputStream fOS = new FileOutputStream("s20220120.txt")) {
+            byte[] bA = bRead.readLine().getBytes();
+            fOS.write(bA);
+        }
+        try(FileInputStream fIS = new FileInputStream("s20220120.txt")) {
+            byte[] bA = fIS.readNBytes(fIS.available()-1);
+            System.out.println(new String(bA));
+        }
+    }
+}
