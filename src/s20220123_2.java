fb8491d4348b2c55e961f6999d6818d9894ef71d
laava <laavart11@gmail.com>
2022-01-23 17:06:51 +0530

    Commit

--- src/s20220123_2.java
+++ src/s20220123_2.java
@@ -13,28 +13,48 @@ public class s20220123_2 {
         ArrayDeque<Integer> a = new ArrayDeque<>(20);
         LinkedList<Integer> b = new LinkedList<>(List.of(2, 99, -6));
 
+        /**
+         * It has methods from Collection class
+         * such as, add(), addAll(), remove(), removeAll(), retainAll(), replaceAll(), etc.
+         */
         a.addAll(b);
 
+        /**
+         * Adding from both ends is possible
+         * These functions throw Exceptions
+         */
         a.add(3);
         a.addFirst(20);
         a.addLast(100);
-
+        /**
+         * These functions do NOT throw Exceptions
+         */
         a.offer(6);
         a.offerFirst(3);
         a.offerLast(15);
-
+        /**
+         * To use as a stack
+         */
         a.push(65);
 
+        System.out.println(a);
+
+        /**
+         * Deleting from both ends is possible
+         * These functions throw Exceptions
+         */
         a.remove();
         a.removeFirst();
         a.removeLast();
-
+        /**
+         * These functions do NOT throw Exceptions
+         */
         a.poll();
         a.pollFirst();
         a.pollLast();
-
+        /**
+         * To use as a stack
+         */
         a.pop();
-
-
     }
 }
