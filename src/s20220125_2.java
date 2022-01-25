6319ceeca184eeb86af2e93fd1667f7bc6eb6819
laava <laavart11@gmail.com>
2022-01-25 09:18:03 +0530

    Commit

---
+++ src/s20220125_2.java
@@ -0,0 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Properties inherits from Hashtable
+ * It is not generic, and both keys & values are STRING
+ */
+public class s20220125_2 {
+    public static void main(String... Args) throws IOException {
+        Properties p = new Properties();
+
+        p.setProperty("Brand", "Dell");
+        p.setProperty("CPU", "Intel i5 8th Gen");
+        p.setProperty("GPU", "Nvidia GTX 1050");
+        p.setProperty("RAM", "16 GB");
+        p.setProperty("Colour", "Black");
+        System.out.println(p);
+
+        /**
+         * Stores all the entries to a file
+         */
+        p.store(new FileOutputStream("OutFiles\\s20220125_2.txt"), "My Laptop");
+
+        /**
+         * Stores all the entries to XML file
+         */
+        p.storeToXML(new FileOutputStream("OutFiles\\s20220125_2.xml"), "My Laptop");
+    }
+}
