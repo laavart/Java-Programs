b44321854a1f8c22638bbf60bbbb077782f93bd0
laava <laavart11@gmail.com>
2022-01-31 06:59:52 +0530

    Commit

--- src/s20220130.java
+++ src/s20220130.java
@@ -1,10 +1,18 @@
 import java.sql.*;
 
+/**
+ * JDBC
+ * Java DataBase Connectivity
+ */
 public class s20220130 {
     public static void main(String[] Args) {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection con = DriverManager.getConnection("jdbc:sqlite:C:/Users/laava/DataGripProjects/SQLite/TJDBC.sqlite");
+            /**
+             * Statement
+             * We can also use PreparedStatement
+             */
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM Student;");
             while(rs.next()){
