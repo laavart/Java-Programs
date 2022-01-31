d1967f1d9002ece3afcbc5427754d79878993b4f
laava <laavart11@gmail.com>
2022-01-31 06:11:24 +0530

    Commit

---
+++ src/s20220130.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class s20220130 {
+    public static void main(String[] Args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:C:/Users/laava/DataGripProjects/SQLite/TJDBC.sqlite");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Student;");
+            while(rs.next()){
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+                System.out.println();
+            }
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
