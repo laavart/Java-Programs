50e38e63b4b7f98af5ae58c004f47aa20c0735b7
laava <laavart11@gmail.com>
2022-01-31 13:19:36 +0530

    Commit

--- src/s20220130.java
+++ src/s20220130.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class s20220130 {
     public static void main(String[] Args) {
         try {
-            Class.forName("org.sqlite.JDBC");
+            Class.forName("org.sqlte.JDBC");
             Connection con = DriverManager.getConnection("jdbc:sqlite:C:/Users/laava/DataGripProjects/SQLite/TJDBC.sqlite");
             /**
              * Statement
