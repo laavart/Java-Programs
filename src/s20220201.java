16cf54f1e6bc7bdd6dc467ed27f4533720b10378
laava <laavart11@gmail.com>
2022-02-01 08:12:12 +0530

    Commit

---
+++ src/s20220201.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+
+public class s20220201 {
+    public static void main(String[] Args){
+        MyFrame4 f = new MyFrame4();
+        f.setVisible(true);
+    }
+}
+
+class MyFrame4 extends Frame implements TextListener{
+
+    TextField tf;
+    Label l1, l2;
+
+    MyFrame4(){
+
+        super("TextField Demo");
+        setLayout(new FlowLayout());
+        setSize(800,450);
+
+        tf = new TextField("Enter Something...", 7);
+        tf.addTextListener(this);
+
+        l1 = new Label("Waiting...");
+        l2 = new Label("Waiting to Press Enter...");
+
+        add(tf);
+        add(l1,0);
+        add(l2);
+    }
+
+    @Override
+    public void textValueChanged(TextEvent e) {
+        String s = tf.getText();
+        l1.setText( s.equals("") ? "_Blank_" : s );
+
+    }
+}
