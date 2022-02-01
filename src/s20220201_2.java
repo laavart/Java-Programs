46f8237ddf8273f2f6bcc358d64038d0c5bc5b8b
laava <laavart11@gmail.com>
2022-02-01 08:47:07 +0530

    Commit

--- src/s20220201_2.java
+++ src/s20220201_2.java
@@ -5,10 +5,24 @@ import java.awt.*;
  */
 public class s20220201_2 {
     public static void main(String[] Args) {
-
+        MyFrame5 f = new MyFrame5();
+        f.setVisible(true);
     }
 }
 
 class MyFrame5 extends Frame{
 
+    TextArea ta;
+
+    MyFrame5(){
+        super("TextArea Demo");
+        setLayout(new FlowLayout());
+        setSize(800,450);
+
+        ta = new TextArea("TextArea", 10, 20);
+        ta.append(" is Great!");
+        ta.setCursor(new Cursor(2));
+
+        add(ta);
+    }
 }
