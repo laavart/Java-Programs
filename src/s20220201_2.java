57bad86f32ac4f49beb0b39aa1522271e3b12f95
laava <laavart11@gmail.com>
2022-02-01 21:11:19 +0530

    Commit

--- src/s20220201_2.java
+++ src/s20220201_2.java
@@ -1,7 +1,10 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * TextArea
+ * Dimension
  */
 public class s20220201_2 {
     public static void main(String[] Args) {
@@ -13,16 +16,38 @@ public class s20220201_2 {
 class MyFrame5 extends Frame{
 
     TextArea ta;
+    Button b;
 
     MyFrame5(){
         super("TextArea Demo");
         setLayout(new FlowLayout());
-        setSize(800,450);
+        Dimension d = new Dimension(800,450);
+        setSize(d);
 
         ta = new TextArea("TextArea", 10, 20);
         ta.append(" is Great!");
         ta.setCursor(new Cursor(2));
 
+        /**
+         * getCaretPosition() returns the position of the cursor
+         */
+
+        b = new Button("Apply");
+        b.addActionListener(e -> ta.setText(ta.getSelectedText()));
+
+        /**
+         * Same as:
+         *      b.addActionListener(new ActionListener(){
+         *
+         *          @Override
+         *          public void actionPerformed(ActionEvent e){
+         *              ta.setText(ta.getSelectedText())
+         *          }
+         *      });
+         */
+
         add(ta);
+        add(b);
+
     }
 }
