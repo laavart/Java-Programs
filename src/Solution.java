789518f06a52c30e236475455ea49b7a9b28102c
laava <laavart11@gmail.com>
2022-07-04 09:05:22 +0530

    Commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class Solution {
-    static int function(String s) {
-        final List<Character> a = new ArrayList<>(List.of('I', 'V', 'X', 'L', 'C', 'D', 'M'));
-        int l = s.length();
-        int r = 0;
+    static String function(String ...s) {
+        StringBuilder r = new StringBuilder();
         byte t = 0;
-        while (l-- > 0) {
-            char c = s.charAt(l);
-            switch (c) {
-                case 'I' -> {
-                    if (t > a.indexOf('I')) r-=1;
-                    else r+=1;
-                }
-                case 'V' -> {
-                    r += 5;
-                    t = 1;
-                }
-                case 'X' -> {
-                    if (t > a.indexOf('X')) r -= 10;
-                    else {
-                        r += 10;
-                        t = 2;
-                    }
-                }
-                case 'L' -> {
-                    r += 50;
-                    t = 3;
-                }
-                case 'C' -> {
-                    if (t > a.indexOf('C')) r -= 100;
-                    else {
-                        r += 100;
-                        t = 4;
-                    }
-                }
-                case 'D' -> {
-                    r += 500;
-                    t = 5;
-                }
-                case 'M' -> {
-                    r += 1000;
-                    t = 6;
-                }
-            }
+        for (var x : s) if (t < x.length()) t = (byte) x.length();
+        for (int k = 0; k < t; k++) {
+            char c;
+            if (s[0].length() > k) c = s[0].charAt(k);
+            else return r.toString();
+            for (int i = 1; i < s.length; i++) if (s[i].length() <= k || s[i].charAt(k) != c) return r.toString();
+            r.append(c);
         }
-        return r;
+        return r.toString();
     }
 
     public static void main(String[] Args) {
-        while (true)
-            System.out.println(Solution.function(
-                    new Scanner(System.in).nextLine()
-            ));
+        System.out.println("Result:\n"+Solution.function("", "b"));
     }
 }
